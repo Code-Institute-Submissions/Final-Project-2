ff68bd6c7ef265ee4fbedd3ab85a03e3ed513ca2
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-09-26 23:00:58 +0000

    Added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HVl5sCLMHMIxghC3wYlzhVt4nqkmCtKEEvez0zjFzyJ2pCZr0TopGPpojZo4JkjPDty7HInLrlu4pOWjfj0XQlI005P2WWHwJ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
