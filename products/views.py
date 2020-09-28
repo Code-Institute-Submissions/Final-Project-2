ba310893569163479879b63c2b2ba0e57586ee3c
Vanitha-krishnan <vanitha-krishnan@outlook.com>
2020-09-28 11:22:24 +0000

    Product admin - Finished add product functionality

--- products/views.py
+++ products/views.py
@@ -29,7 +29,7 @@ def all_products(request):
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-            
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -70,7 +70,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Add a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+    
     template = 'products/add_product.html'
     context = {
         'form': form,
